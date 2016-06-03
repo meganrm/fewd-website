4561597dab8505834c7fc3100ad0120a7553ac88
meganrm <meganrm@gmail.com>
2016-06-03 14:49:39 -0700

    newest item

---
+++ pgrn/DOMfunctions.js
@@ -0,0 +1,41 @@
+var body = document.querySelector("body");
+
+function newTag(tagName, options) {
+  for (var i = 0; i < arguments.length; i++) {
+    var arg = arguments[i];
+  }
+}
+
+  function newTagText(tagType, parent, text){
+    var elementName=document.createElement(tagType);
+    parent.appendChild(elementName);
+    elementName.textContent=text;
+    return elementName;
+  }
+
+
+  function newTagjQuery(tagType, parent, text, newclass){
+    var elementName=$('<'+tagType+'>');
+    elementName.appendTo(parent).text(text).addClass(newclass);
+    return elementName;
+  }
+
+  function newlistitem(listid, text, id, newclass) {
+    var li= newTagjQuery('li', listid, text, newclass);
+    li.attr('id', id);
+    return li;
+  }
+
+
+  function newImage(parent, src, height, width){
+    var elementName=document.createElement("img");
+    parent.appendChild(elementName);
+    elementName.setAttribute("src", src);
+    elementName.setAttribute("height", height);
+    elementName.setAttribute("width", width);
+    return elementName;
+  }
+
+function convertToNumber(input){
+  return parseInt(input.value);
+}
